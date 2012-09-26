b7bd80dbc7e3b7786e49154508b0b9915d9b7737
Chris Griego <cgriego@gmail.com>
2012-09-26 17:23:10 -0500

    Initial cookbook skeleton that can be successfully uploaded

---
+++ Gemfile
@@ -0,0 +1,4 @@
+source "https://rubygems.org"
+
+gem "chef", "< 10.14.0" # minitest-chef-handler is not yet compatible
+gem "spiceweasel"
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name             "apps-database"
+maintainer       "getaroom"
+maintainer_email "devteam@roomvaluesteam.com"
+license          "MIT"
+description      "Configures databases for Apps"
+long_description IO.read(File.join(File.dirname(__FILE__), "README.md"))
+version          "1.0.0"
+
+depends "apps"
+depends "database"
+
+supports "debian"
+supports "ubuntu"
+
+recipe "apps-database", "Configures databases for apps."
